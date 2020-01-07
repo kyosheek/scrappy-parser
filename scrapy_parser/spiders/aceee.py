12e769174aa9944734c6305ceedb752ac43856d4
kyosheek <31596992+kyosheek@users.noreply.github.com>
2020-01-08 01:37:47 +0300

    Parse every article
    Теперь парсер проходит по каждой ссылке на первой странице и по всем, кроме первой, на следующих (заглавная новость закреплена)
    
    Блок с рекламой не парсится
    
    Лишние символы из текста статьи удаляются

--- scrapy_parser/spiders/aceee.py
+++ scrapy_parser/spiders/aceee.py
@@ -1,63 +1,41 @@
+from datetime import datetime
+
 import scrapy
 
 class AceeeSpider(scrapy.Spider):
     name = "aceee"
     
-#    start_urls = [
-#            'https://aceee.org/news-blog'
-#            ]
-
+    now = datetime.now()
+    datestring = now.strftime("%Y-%m-%d")
+    
     start_urls = [
-        'https://aceee.org/blog/2020/01/2019-and-2020-good-bad-and-ugly'
-    ]
+            'https://aceee.org/news-blog'
+            ]
 
-#    def parse(self, response):
-#        for article in response.css('.news-content-box h2'):
-#            yield {
-#                'title': article.css('a::text').get(),
-#                'link': article.css('a::attr(href)').get()
-#            }
-#
-#        next_page = response.css('li.next a::attr(href)').get()
-
-#        if next_page is not None:
-#            yield response.follow(next_page, callback=self.parse)
-
+    flag = False
     def parse(self, response):
-        text = " ".join(response.xpath('//article/div/div/div/div//text()').extract())
+        hrefs = response.css('.news-content-box h2 a::attr(href)').getall()
+        if self.flag is False:
+            self.flag = True
+            for href in hrefs:
+                yield response.follow(href, self.parse_article)
+        else:
+            for i in range(1, len(hrefs)):
+                yield response.follow(hrefs[i], self.parse_article)
+        
+        next_page = response.css('li.next a::attr(href)').get()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_article(self, response):
+        text = (" ".join(response.xpath('//article/div/div/div/div//text()[not(ancestor::*[@class="form-submit"])]').extract())).replace("\xa0", "")
         yield {
             'title': response.xpath('//meta[@name="dcterms.title"]/@content').get(),
             'pubdate': response.xpath('//meta[@name="dcterms.date"]/@content').get(),
-            'datestring': '2020-01-08',
+            'datestring': self.datestring,
             'categories': response.xpath('//div[@class="pane-content"]/ul[@class="views-summary"]/li/a/text()').extract(),
             'article_body': text,
             'tags': response.xpath('//div[@class="views-field views-field-term-node-tid"]/span[@class="field-content"]/i/a/text()').extract(),
             'external_links': response.xpath('//article/div/div/div/div//a/@href').extract()
         }
-
-# заголовок: response.xpath("//meta[@name='dcterms.title']/@content").get()
-
-# ссылка: response.xpath("//link[@rel='shortlink']/@href").get()
-
-# дата публикации: response.xpath("//meta[@name='dcterms.date']/@content").get()
-
-# категории: response.xpath('//div[@class="pane-content"]/ul[@class="views-summary"]/li/a/text()').extract()
-
-# текст статьи: response.xpath('//article/div/div/div/div//text()').extract()
-#   concat из массива в строку
-
-# теги: response.xpath('//div[@class="views-field views-field-term-node-tid"]/span[@class="field-content"]/i/a/text()').extract()
-
-# ссылки: response.xpath('//article/div/div/div/div//a/@href').extract()
-
-# ------------------------------------------------------------------------------
-
-# Заголовки
-# response.css('.news-content-box h2 a::text').getall()
-
-# Ссылки
-# response.css('.news-content-box h2 a::attr(href)').getall()
-
-# Следующая страница
-# response.css('li.next a::attr(href)').get()
-
